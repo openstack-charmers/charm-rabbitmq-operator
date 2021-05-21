a8746812fe3f1ebcd835687e6a7fde20542be2e7
David Ames <david.ames@canonical.com>
2021-05-21 11:12:18 -0700

    Almost complete AMQP relation

---
+++ lib/charms/interface_rabbitmq_amqp/v0/rabbitmq.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+# Copyright 2021 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""RabbitMQAMQPProvides and Requires module"""
+
+import logging
+import requests
+
+from ops.framework import (
+    StoredState,
+    EventBase,
+    ObjectEvents,
+    EventSource,
+    Object)
+
+logger = logging.getLogger(__name__)
+
+
+class HasAMQPServersEvent(EventBase):
+    """Has AMQPServers Event."""
+    pass
+
+
+class ReadyAMQPServersEvent(EventBase):
+    pass
+
+
+class RabbitMQAMQPServerEvents(ObjectEvents):
+    has_amqp_servers = EventSource(HasAMQPServersEvent)
+    ready_amqp_servers = EventSource(ReadyAMQPServersEvent)
+
+
+class RabbitMQAMQPRequires(Object):
+    """
+    RabbitMQAMQPRequires class
+    """
+
+    on = RabbitMQAMQPServerEvents()
+    _stored = StoredState()
+
+    def __init__(self, charm, relation_name):
+        super().__init__(charm, relation_name)
+        self.charm = charm
+        self.relation_name = relation_name
+        self.framework.observe(
+            self.charm.on[relation_name].relation_joined, self._on_amqp_relation_joined
+        )
+        self.framework.observe(
+            self.charm.on[relation_name].relation_changed, self._on_amqp_relation_changed
+        )
+        self.framework.observe(
+            self.charm.on[relation_name].relation_broken, self._on_amqp_relation_broken
+        )
+
+    @property
+    def _amqp_rel(self):
+        return self.framework.model.get_relation(self.relation_name)
+
+    def _on_amqp_relation_joined(self, event):
+        logging.debug("RabbitMQAMQPRequires on_joined")
+        self.event = event
+        self.on.has_amqp_servers.relation_event = event
+        self.on.has_amqp_servers.emit()
+        # TODO Move to charm code once the emit has this event attached
+        self.request_access(event, self.charm.username, self.charm.vhost)
+
+    def _on_amqp_relation_changed(self, event):
+        logging.debug("RabbitMQAMQPRequires on_changed")
+        self.event = event
+        self.request_access(event, self.charm.username, self.charm.vhost)
+        if self.password(event):
+            self.on.ready_amqp_servers.emit()
+
+    def _on_amqp_relation_broken(self, event):
+        # TODO clear data on the relation
+        logging.debug("RabbitMQAMQPRequires on_departed")
+
+    def password(self, event):
+        return event.relation.data[self.charm.app].get("password")
+
+    def request_access(self, event, username, vhost):
+        logging.debug("Requesting AMQP user and vhost")
+        event.relation.data[self.charm.app]['username'] = username
+        event.relation.data[self.charm.app]['vhost'] = vhost
+
+
+class HasAMQPClientsEvent(EventBase):
+    """Has AMQPClients Event."""
+    pass
+
+
+class ReadyAMQPClientsEvent(EventBase):
+    pass
+
+
+class RabbitMQAMQPClientEvents(ObjectEvents):
+    has_amqp_clients = EventSource(HasAMQPClientsEvent)
+    ready_amqp_clients = EventSource(ReadyAMQPClientsEvent)
+
+
+class RabbitMQAMQPProvides(Object):
+    """
+    RabbitMQAMQPProvides class
+    """
+
+    on = RabbitMQAMQPClientEvents()
+    _stored = StoredState()
+
+    def __init__(self, charm, relation_name):
+        super().__init__(charm, relation_name)
+        self.charm = charm
+        self.relation_name = relation_name
+        self.framework.observe(
+            self.charm.on[relation_name].relation_joined, self._on_amqp_relation_joined
+        )
+        self.framework.observe(
+            self.charm.on[relation_name].relation_changed, self._on_amqp_relation_changed
+        )
+        self.framework.observe(
+            self.charm.on[relation_name].relation_broken, self._on_amqp_relation_broken
+        )
+
+    @property
+    def _amqp_rel(self):
+        return self.framework.model.get_relation(self.relation_name)
+
+    def _on_amqp_relation_joined(self, event):
+        logging.debug("RabbitMQAMQPProvides on_joined")
+        self.on.has_amqp_clients.emit()
+
+    def _on_amqp_relation_changed(self, event):
+        logging.debug("RabbitMQAMQPProvides on_changed")
+        # Validate data on the relation
+        if self.username(event) and self.vhost(event):
+            self.on.ready_amqp_clients.emit()
+            if self.charm.unit.is_leader():
+                self.set_amqp_credentials(
+                    event,
+                    self.username(event),
+                    self.vhost(event))
+
+    def _on_amqp_relation_broken(self, event):
+        logging.debug("RabbitMQAMQPProvides on_departed")
+        # TODO clear data on the relation
+
+    def username(self, event):
+        return event.relation.data[self._amqp_rel.app].get("username")
+
+    def vhost(self, event):
+        return event.relation.data[self._amqp_rel.app].get("vhost")
+
+    def set_amqp_credentials(self, event, username, vhost):
+        # TODO: Can we move this into the charm code?
+        logging.debug("Setting amqp connection information")
+        try:
+            if not self.charm.does_vhost_exist(vhost):
+                self.charm.create_vhost(vhost)
+            if not self.charm.does_vhost_exist(username):
+                password = self.charm.create_user(username)
+                self.charm.set_user_permissions(username, vhost)
+                event.relation.data[self.charm.app]["password"] = password
+            event.relation.data[self.charm.app]["hostname"] = self.charm.hostname
+        except requests.exceptions.ConnectionError as e:
+            logging.warning("Rabbitmq is not ready. Defering. Errno: {}".format(e.errno))
+            event.defer()
+        # TODO TLS Support. The reactive interfaces set ssl_port and ssl_ca
--- src/charm.py
+++ src/charm.py
@@ -15,20 +15,25 @@ develop a new k8s charm using the Operator Framework:
 import logging
 import pwgen
 import rabbitmq_admin
+import requests
 from typing import Union
 
 from charms.nginx_ingress_integrator.v0.ingress import IngressRequires
+from charms.interface_rabbitmq_amqp.v0.rabbitmq import RabbitMQAMQPProvides
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, WaitingStatus
 import interface_rabbitmq_operator_peers
-import interface_rabbitmq_operator_amqp_provider
 
 logger = logging.getLogger(__name__)
 
 RABBITMQ_CONTAINER = "rabbitmq"
 RABBITMQ_SERVER_SERVICE = "rabbitmq-server"
+
+
+class RabbitMQAdminAPINotReady(Exception):
+    pass
 
 
 class RabbitMQOperatorCharm(CharmBase):
@@ -39,21 +44,25 @@ class RabbitMQOperatorCharm(CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
+
+        # TODO on_relation_created
         self.framework.observe(self.on.rabbitmq_pebble_ready, self._on_rabbitmq_pebble_ready)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
         self.framework.observe(self.on.get_operator_info_action, self._on_get_operator_info_action)
+        self.framework.observe(self.on.update_status, self._on_update_status)
         # Peers
         self.peers = interface_rabbitmq_operator_peers.RabbitMQOperatorPeers(self, "peers")
         self.framework.observe(self.peers.on.has_peers, self._on_has_peers)
-        # AMQP Provider
-        self.amqp_provider = (
-            interface_rabbitmq_operator_amqp_provider.RabbitMQAMQPProvider(self, "amqp"))
+        # AMQP Provides
+        self.amqp_provider = (RabbitMQAMQPProvides(self, "amqp"))
         # self.framework.observe(self.amqp_provider.on.has_peers, self._on_has_amqp_clients)
         self.framework.observe(
             self.amqp_provider.on.ready_amqp_clients, self._on_ready_amqp_clients)
 
+        self._stored.set_default(users={})
         self._stored.set_default(operator={})
         self._stored.set_default(enabled_plugins=[])
+        self.api_not_ready = RabbitMQAdminAPINotReady
 
         self.ingress_mgmt = IngressRequires(self, {
             "service-hostname": "rabbitmq-management.juju",
@@ -70,10 +79,12 @@ class RabbitMQOperatorCharm(CharmBase):
         container.add_layer("rabbitmq", self._rabbitmq_layer(), combine=True)
         # Autostart any services that were defined with startup: enabled
         if not container.get_service(RABBITMQ_SERVER_SERVICE).is_running():
+            logging.info("Autostarting rabbitmq")
             container.autostart()
-        self.unit.status = ActiveStatus()
+        else:
+            logging.debug("Rabbitmq service is running")
 
-    def _on_config_changed(self, _):
+    def _on_config_changed(self, event):
         """Config changed.
         """
         management_plugin = self.config["management_plugin"]
@@ -105,8 +116,21 @@ class RabbitMQOperatorCharm(CharmBase):
             container.start(RABBITMQ_SERVER_SERVICE)
             logging.info("Restarted rabbitmq-service service")
 
-        # All is well, set an ActiveStatus
-        self.unit.status = ActiveStatus()
+        if (not self.peers.operator_user_created and
+                self.unit.is_leader()):
+            # Generate the operator user/password
+            logging.info("Attempting to initilize operator user.")
+            try:
+                self._initialize_operator_user()
+            except requests.exceptions.HTTPError as e:
+                if e.errno == 401:
+                    logging.error("Athorization failed")
+                    raise e
+            except requests.exceptions.ConnectionError as e:
+                logging.warning("Rabbitmq is not ready. Defering. Errno: {}".format(e.errno))
+                event.defer()
+
+        self._on_update_status(event)
 
     def _rabbitmq_layer(self):
         return {
@@ -126,15 +150,74 @@ class RabbitMQOperatorCharm(CharmBase):
         """Event handler on has peers.
         """
         logging.info("Peer relation instantiated.")
-        # Generate the operator user/password
         if (not self.peers.operator_user_created and
                 self.unit.is_leader()):
-            self._initialize_operator_user()
+
+            logging.info("Attempting to initilize from on has peers.")
+            # Generate the operator user/password
+            try:
+                self._initialize_operator_user()
+            except requests.exceptions.HTTPError as e:
+                if e.errno == 401:
+                    logging.error("Athorization failed")
+                    raise e
+            except requests.exceptions.ConnectionError as e:
+                logging.warning(
+                    "Rabbitmq is not ready. Defering. Errno: {}".format(e.response.status_code))
+                event.defer()
 
     def _on_ready_amqp_clients(self, event):
-        """WIP Event handler on AMQP clients ready.
+        """Event handler on AMQP clients ready.
         """
+        logging.warning("Handled in the interface")
         pass
+
+    @property
+    def amqp_rel(self):
+        return self.framework.model.get_relation("amqp")
+
+    @property
+    def amqp_bind_address(self):
+        return str(
+            self.model.get_binding(self.amqp_rel).network.bind_address)
+
+    def does_user_exist(self, user):
+        api = self._get_admin_api(self._operator_user, self._operator_password)
+        try:
+            return api.get_user(user)
+        except requests.exceptions.HTTPError as e:
+            if e.response.status_code == 404:
+                return False
+            else:
+                raise e
+
+    def does_vhost_exist(self, vhost):
+        api = self._get_admin_api(self._operator_user, self._operator_password)
+        try:
+            return api.get_vhost(vhost)
+        except requests.exceptions.HTTPError as e:
+            if e.response.status_code == 404:
+                return False
+            else:
+                raise e
+
+    def create_user(self, user):
+        api = self._get_admin_api(self._operator_user, self._operator_password)
+        _password = pwgen.pwgen(12)
+        api.create_user(user, _password)
+        return _password
+
+    def set_user_permissions(self, user, vhost, configure=".*", write=".*", read=".*"):
+        api = self._get_admin_api(self._operator_user, self._operator_password)
+        return api.create_user_permission(user, vhost, configure=configure, write=write, read=read)
+
+    def create_vhost(self, vhost):
+        api = self._get_admin_api(self._operator_user, self._operator_password)
+        try:
+            return api.create_vhost(vhost)
+        except requests.exceptions.HTTPError as e:
+            logging.error(e)
+            raise e
 
     def _enable_plugin(self, plugin):
         if plugin not in self._stored.enabled_plugins:
@@ -145,26 +228,42 @@ class RabbitMQOperatorCharm(CharmBase):
             self._stored.enabled_plugins.remove(plugin)
 
     @property
+    def hostname(self):
+        # For now we will use http://localhost
+        # TODO get service or ingress URL for the pod
+        # Internal
+        # f"{self.unit.name}.{self.app.name}-endpoints.{self.model.name}.svc.cluster.local"
+        # External
+        # f"{self.unit.name}.{self.app.name}.{self.model.name}.svc.cluster.local"
+        # return "localhost"
+        return self.amqp_bind_address
+
+    @property
     def _rabbitmq_mgmt_url(self):
         # For now we will use http://localhost
         # TODO get service or ingress URL for the pod
+        # Internal
+        # f"{self.unit.name}.{self.app.name}-endpoints.{self.model.name}.svc.cluster.local"
+        # External
+        # f"{self.unit.name}.{self.app.name}.{self.model.name}.svc.cluster.local"
         return "http://localhost:15672"
 
     @property
     def _operator_password(self) -> Union[str, None]:
+        # TODO: For now we are storing this in the peer relation so that all
+        # peers have access to it. Move to Kubernetes secrets.
         if not self.peers.operator_password and self.unit.is_leader():
             self.peers.set_operator_password(pwgen.pwgen(12))
         return self.peers.operator_password
 
+    def _get_admin_api(self, username, password):
+        return rabbitmq_admin.AdminAPI(
+            url=self._rabbitmq_mgmt_url, auth=(username, password))
+
     def _initialize_operator_user(self):
-        if not self._operator_password:
-            logging.warning(
-                "Called initialize the operator user too early. "
-                "Defer.")
-            return
         logging.info("Initializing the operator user.")
         # Use guest to create operator user
-        api = rabbitmq_admin.AdminAPI(url=self._rabbitmq_mgmt_url, auth=("guest", "guest"))
+        api = self._get_admin_api("guest", "guest")
         api.create_user(self._operator_user, self._operator_password, tags=["administrator"])
         api.create_user_permission(self._operator_user, vhost="/")
         self.peers.set_operator_user_created(self._operator_user)
@@ -206,6 +305,14 @@ loopback_users = {loopback_users}
         }
         event.set_results(data)
 
+    def _on_update_status(self, event):
+        if self.peers.operator_user_created:
+            self.unit.status = ActiveStatus()
+        else:
+            self.unit.status = WaitingStatus("Waiting to initilaize operator user")
+
 
 if __name__ == "__main__":
+    # Note: use_juju_for_storage=True required per
+    # https://github.com/canonical/operator/issues/506
     main(RabbitMQOperatorCharm, use_juju_for_storage=True)
